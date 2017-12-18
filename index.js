4ab9ffbae21deee52f10aa81a35add656bdfa18a
Kazumi Karbowski <kazkazinus@gmail.com>
2017-12-18 05:05:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,6 @@ function increaseRankBy(n) {
   
  
 function deepestChild(){
-  var children = document.querySelectorAll('#grand-node');
+  var children = document.getElementById('#grand-node').querySelectorAll('div');
   return children[children.length -1];
 } 
